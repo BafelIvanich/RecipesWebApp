2d87819c7c39afc5b3eb674619599d8c83772652
Блаблакар Ниторон <uluu33697@gmail.com>
2025-04-24 20:26:47 +0300

    Added fridge image logic

--- RecipesApp/Controllers/FilesController.cs
+++ RecipesApp/Controllers/FilesController.cs
@@ -10,19 +10,31 @@ using RecipesApp.Extensions;
 using System.Net.WebSockets;
 using System.Runtime.InteropServices;
 using RecipesApp.ViewModels;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
 
 namespace RecipesApp.Controllers
 {
     public class FilesController : Controller
     {
-        private readonly string pythonPath = "/usr/bin/python3";
-        private readonly string pythonScriptPath = "/path/";
+        private readonly IWebHostEnvironment _env;
+        private readonly string pythonPath = null;
+        private readonly string pythonScriptPath = null;
 
         private readonly ILogger<FilesController> _logger;
 
-        public FilesController(ILogger<FilesController> logger)
+        public FilesController(ILogger<FilesController> logger,IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
+
+            pythonPath = Path.Combine(_env.ContentRootPath, "PythonScripts", "venv","Scripts","python.exe");
+            pythonScriptPath = Path.Combine(_env.ContentRootPath, "PythonScripts","process_image.py");
+
+            if (!System.IO.File.Exists(pythonScriptPath))
+            {
+                 _logger.LogWarning("Python script not found at configured path: {ScriptPath}", pythonScriptPath);
+            }
         }
 
         [HttpPost]
@@ -33,15 +45,83 @@ namespace RecipesApp.Controllers
                 TempData["UploadError"] = "Пожалуйста, выберите файл изображения";
                 return RedirectToAction("Fridge","Recipes");
             }
+                string tempFilePath = Guid.NewGuid().ToString() + Path.GetExtension(fridgeImage.FileName);
+                string pythonResult = null;
+                string pythonError = null;
+
             try
             {
+
+                using (var stream = new FileStream(tempFilePath, FileMode.Create))
+                {
+                    await fridgeImage.CopyToAsync(stream);
+                }
                 _logger.LogInformation($"Изображение {fridgeImage.FileName}");
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = pythonPath,
+                    ArgumentList = { pythonScriptPath, tempFilePath },
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    StandardOutputEncoding = System.Text.Encoding.UTF8,
+                    StandardErrorEncoding = System.Text.Encoding.UTF8
+                };
+                _logger.LogInformation($"Скрипт запущен");
+
+                startInfo.EnvironmentVariables["PYTHONIOENCODING"] = "utf-8";
+
+                using (var process = Process.Start(startInfo))
+                {
+                    if (process == null) throw new InvalidOperationException("ошибка запуска");
+
+                    var outputTask = process.StandardOutput.ReadToEndAsync();
+                    var errorTask = process.StandardError.ReadToEndAsync();
+                    bool exited = await Task.Run(() => process.WaitForExit(60000));
+
+                    if (!exited)
+                    {
+                        _logger.LogWarning("таймаут");
+                        try { process.Kill(true); } catch { }
+                        throw new TimeoutException($"Время обработки запроса истекло");
+                    }
+                    pythonResult = await outputTask;
+                    pythonError = await errorTask;
+
+                    if (!string.IsNullOrEmpty(pythonError)) _logger.LogWarning($"python errors:{pythonError}");
+                    if (!string.IsNullOrEmpty(pythonResult)) _logger.LogInformation($"python script returned:{pythonResult}");
+                    string outputResult = pythonResult.Trim();
+                    string errorResult = pythonError.Trim();
+                    _logger.LogInformation($"получено: {outputResult}");
+                    _logger.LogInformation($"ошибка: {errorResult}");
+                }
+
                 return RedirectToAction("Fridge", "Recipes");
             }
             catch (Exception ex)
             {
                 return RedirectToAction("Fridge", "Recipes");
             }
+            finally
+            {
+                if (tempFilePath != null && System.IO.File.Exists(tempFilePath))
+                {
+                    try
+                    {
+                        System.IO.File.Delete(tempFilePath);
+                        _logger.LogInformation($"изображение {tempFilePath} удалено");
+                    }
+                    catch (IOException ioEx)
+                    {
+                        _logger.LogInformation($"{ioEx}");
+                    }
+                }
+
+            }
+
+            
         }
 
     }
---
+++ RecipesApp/PythonScripts/process_image.py
@@ -0,0 +1,65 @@
+﻿#-*- coding: utf-8 -*-
+
+import argparse
+import sys
+import openai
+import requests, base64
+from openai import AsyncOpenAI
+
+api_key = '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'
+invoke_url = "https://integrate.api.nvidia.com/v1/chat/completions"
+stream = False
+
+client = AsyncOpenAI(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key=api_key
+)
+
+
+def process_image(image_path):
+    with open(image_path,'rb') as f:
+        image_b64 = base64.b64encode(f.read()).decode()
+
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Accept": "text/event-stream" if stream else "application/json"
+        }
+
+        payload = {
+            "model": 'meta/llama-4-maverick-17b-128e-instruct',
+            "messages": [
+                {
+                    "role": "user",
+                    "content": f'{"Перечисли все продукты, изображенные на картинке через запятую. Больше ничего не пиши."} <img src="data:image/png;base64,{image_b64}" />'
+                }
+            ],
+            "max_tokens": 512,
+            "temperature": 0.2,
+            "top_p": 1.00,
+            "stream": stream
+        }
+
+    response = requests.post(invoke_url, headers=headers, json=payload)
+    response_json = response.json()
+    if response.status_code == 402:
+        raise openai.APIStatusError(response=response,message="Token expired",body=None)
+    message_content = response_json['choices'][0]['message']['content']
+
+    if message_content:
+        return message_content
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_path',type=str)
+
+    args = parser.parse_args()
+
+    output_result = process_image(args.image_path)
+
+    print(output_result)
+
+    if output_result.startswith("Error:"):
+        sys.exit(1)
+    else:
+        sys.exit(0)
